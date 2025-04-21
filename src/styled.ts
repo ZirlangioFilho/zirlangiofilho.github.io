2ab0e385dd2b01ee0d6f7c494be97e93248d5091
ZirlangioFilho <zirlangiocurso@gmail.com>
2025-04-21 11:49:39 -0300

    Fix: configuracao do package.json

--- src/styled.ts
+++ src/styled.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
-export const Container = styled.header`
-
-
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `
